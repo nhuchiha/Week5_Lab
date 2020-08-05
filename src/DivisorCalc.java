9e8cf8bff9c85807fed680259f6d30268e0216e8
nhuchiha <nhuchiha2019@gmail.com>
2020-08-05 16:54:48 -0700

    DivisorCalc

--- src/DivisorCalc.java
+++ src/DivisorCalc.java
@@ -1,7 +1,11 @@
 //Exercise #3
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class DivisorCalc 
 {
-
+	private static int firstNum = 0;
+	private static int secondNum = 0; 
 	public static void main(String[] args) 
 	{
 		/*
@@ -17,12 +21,30 @@ public class DivisorCalc
 
 		 */
 		
-		public static gcd(int num1, num2)
+		Scanner scan = new Scanner(System.in);
+		try
 		{
-			
-			
+		System.out.print("Please enter the first positive number: ");
+		firstNum = scan.nextInt();
+		System.out.print("Please enter the seccond positive number: ");
+		secondNum = scan.nextInt();
 		}
-
+		catch(InputMismatchException e)
+		{
+			System.out.println("Not an integer.");
+		}
+		
+		System.out.println("The gcd of " + firstNum + " and " + secondNum + " is " + gcd(firstNum,secondNum));
+	}
+	// find gcd for two input positive integers
+	public static int gcd(int num1, int num2)
+	{
+		if(num2 <= num1 && num1 % num2 == 0)
+			return num2;
+		else if (num1 < num2)
+			return gcd(num2,num1);
+		else 
+			return gcd(num2, num1 % num2); 
 	}
 
 }
