d15f612bb31702de492f96681357dc97c07cf789
nhuchiha <nhuchiha2019@gmail.com>
2020-08-06 08:40:05 -0700

    Update 3 and 4

--- src/DivisorCalc.java
+++ src/DivisorCalc.java
@@ -7,20 +7,7 @@ public class DivisorCalc
 	private static int firstNum = 0;
 	private static int secondNum = 0; 
 	public static void main(String[] args) 
-	{
-		/*
-		 * Design and implement a program that implements Euclid's algorithm for finding the greatest
-			common divisor of two positive integers. The greatest common divisor is the largest integer
-			that divides both values without producing a remainder. In a class called DivisorCalc,
-			define a static method called gcd that accepts two integers, num1 and num2. Create a driver
-			program inside the main method to test your implementation. The recursive algorithm is
-			defined as follows:
-			gcd(num1, num2) is num2 if num2 <= num1 and num2 divides num1
-			gcd(num1, num2) is gcd(num2, num1) if num1 < num2
-			gcd(num1, num2) is gcd(num2, num1%num2) otherwise
-
-		 */
-		
+	{		
 		Scanner scan = new Scanner(System.in);
 		try
 		{
--- src/StringReversal.java
+++ src/StringReversal.java
@@ -1,20 +1,26 @@
 // Exercise 4
+import java.util.Scanner;
 public class StringReversal 
 {
 
 	public static void main(String[] args) 
 	{
-		/*
-		 * Create a class called StringReversal. Inside of it, create a recursive method called
-			reverseString that accepts a String s and returns a new String which is s in reverse
-			order. Create a driver program inside the main method to test your implementation.
-		 */
-
+		//Declare string variables
+		String inputString;
+		
+		//User to enter the input String
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Please enter a string: ");
+		inputString = scan.nextLine();
+		scan.close();
+	
+		//Print out the reversed string
+		System.out.print("The reversed string is: " + reverseString(inputString));
 	}
 	public static String reverseString(String s)
 	{
-		String reverseString ="";
-		
-		return reverseString;
+		if(s.length() == 0)
+			return s;
+		return reverseString(s.substring(1)) + s.charAt(0);
 	}
 }
